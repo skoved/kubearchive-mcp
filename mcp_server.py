c596bb225f6794e5a8d1fe46b247f8b7a2b61e25
skoved <skoved@redhat.com>
2025-07-22 16:13:51 -0400

    add tool to list resources found in kubearchive

--- mcp_server.py
+++ mcp_server.py
@@ -55,6 +55,31 @@ async def logs(apiGroup: str, apiVersion: str, resourceType: str, name: str, nam
     response = await make_request(url, stream=True)
     return response
 
+@mcp.tool()
+async def resources(apiGroup: str, apiVersion: str, resourceType: str, namespace: Optional[str] = None, limit: int = 100, labelSelector: Optional[str] = None):
+    """List the names in a Kubernetes namespace given the api group, api version, resource type and namespace from KubeArchive"""
+    if not resourceType.endswith("s"):
+        resourceType = engine.plural(resourceType.lower())
+    else:
+        resourceType = resourceType.lower()
+
+    apiGroupVersion = f"apis/{apiGroup.lower()}/{apiVersion.lower()}"
+    if (apiGroup.lower() == "core" or apiGroup == "") and apiVersion.lower() == "v1":
+        apiGroupVersion = "api/v1"
+
+    url = ""
+    if namespace is not None:
+        url = f"{API_BASE_URL}/{apiGroupVersion}/namespaces/{namespace.lower()}/{resourceType}"
+    else:
+        url = f"{API_BASE_URL}/{apiGroupVersion}/{resourceType}"
+
+    params = {"limit": limit}
+    if labelSelector is not None:
+        params["labelSelector"] = labelSelector
+
+    response = await make_request(url, data=params)
+    names = [{"name": x["metadata"]["name"], "namespace": x["metadata"]["namespace"], "owner": x["metadata"]["ownerReferences"][0]["name"]} for x in response["items"]]
+    return names
 
 if __name__ == "__main__":
     mcp.run(transport=os.environ.get("MCP_TRANSPORT", "stdio"))
